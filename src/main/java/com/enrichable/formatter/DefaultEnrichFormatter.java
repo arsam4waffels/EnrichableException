8f85c3a4a7c0f5e4d210f9b4f70e7da01271e8bd
Arsam <arsam4waffels@gmail.com>
2026-09-02 11:38:03 +0330

    refactor: instance name change

--- src/main/java/com/enrichable/formatter/DefaultEnrichFormatter.java
+++ src/main/java/com/enrichable/formatter/DefaultEnrichFormatter.java
@@ -13,33 +13,34 @@ public class DefaultEnrichFormatter implements EnrichFormatter {
     public DefaultEnrichFormatter(EnrichConfiguration config) {
         this.config = config;
     }
-
     @Override
     public String format(List<EnrichInformation> list) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("[").append(list.size()).append("-ERRORS]");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[").append(list.size()).append("-ERRORS]");
         for (EnrichInformation info : list) {
-            sb.append("[").append(info.getContext())
+            stringBuilder.append("[").append(info.getContext())
                     .append(":").append(info.getCode()).append("]");
         }
-        sb.append("\n");
-
+        stringBuilder.append("\n");
         for (int i = 0; i < list.size(); i++) {
             EnrichInformation info = list.get(i);
 
             if (config.isShowErrorCount())
-                sb.append("[ERROR-").append(i + 1).append("]");
+                stringBuilder.append("[ERROR-")
+                        .append(i + 1)
+                        .append("]");
 
             if (config.isShowTimestamp())
-                sb.append("[")
+                stringBuilder.append("[")
                         .append(info.getDateTime().format(DATE_TIME_FORMATTER))
                         .append("]");
 
             if (config.isShowErrorLevel())
-                sb.append("[").append(info.getErrorLevel()).append("]");
+                stringBuilder.append("[")
+                        .append(info.getErrorLevel())
+                        .append("]");
 
-            sb.append("[")
+            stringBuilder.append("[")
                     .append(info.getContext())
                     .append(":")
                     .append(info.getCode())
@@ -48,13 +49,13 @@ public class DefaultEnrichFormatter implements EnrichFormatter {
                     .append("\n");
 
             if (config.isShowMetadata())
-                for (Map.Entry<String, String> e : info.getMetadata().entrySet())
-                    sb.append("[")
-                            .append(e.getKey())
+                for (Map.Entry<String, String> stringEntry : info.getMetadata().entrySet())
+                    stringBuilder.append("[")
+                            .append(stringEntry.getKey())
                             .append("=")
-                            .append(e.getValue())
+                            .append(stringEntry.getValue())
                             .append("]\n");
         }
-        return sb.toString();
+        return stringBuilder.toString();
     }
 }
